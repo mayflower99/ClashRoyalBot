aa9ff52a59e1185991b478ad6f8ca5799bf5a23a
mayflower99 <104937613+mayflower99@users.noreply.github.com>
2022-11-10 20:52:32 -0500

    First commit

---
+++ main.py
@@ -0,0 +1,10 @@
+import tensorflow as tfm
+from time import sleep
+from pywinauto.application import Application
+import pywinauto
+import cv2
+import sys
+from clashroyalbot import ClashRoyalBot
+if __name__ == "__main__":
+    clashroyalbot = ClashRoyalBot()
+    clashroyalbot.launch()
